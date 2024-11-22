478d7a265aba8237ad44028e537fcbbd45c9e5fc
Lynn <htinlinn.116900@gmail.com>
2024-11-22 11:34:17 +0700

    updated the rooms to allow for customization

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ io.on('connection', (socket) => {
 
   // Teacher creates and joins a room
   socket.on('createRoom', (roomData) => {
-  const { roomId, subjectCode, section, duration, location, createdAt } = roomData;
+  const { roomId, subjectCode, section, duration, location, createdAt, limitAttendence, maxDistance, lateThreshold } = roomData;
   // console.log(roomData);
 
   // Check if the room already exists
@@ -48,6 +48,9 @@ io.on('connection', (socket) => {
     duration,
     location,
     createdAt,
+    limitAttendence,
+    maxDistance,
+    lateThreshold,
     attendees: [
     ],
   };
